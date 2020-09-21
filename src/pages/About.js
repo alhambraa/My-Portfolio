dfdcdb2228e0b4963dca8424a1c6feefc3079ed5
alhambraa <hambra02@gmail.com>
2020-09-21 19:32:39 +0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,11 +8,10 @@ const About = () => {
                 <div className="col-md-7">
                 <h2 className="featurette-heading text-left">Hai, Nice to Meet You </h2>
                     <br></br>
-                    <p className="text-left text-dark ml-4">
-                        <p>Mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.mollis euismod. Cras mattis consectetur purus sit amet fermentum. </p>
-                        <p>Aenean lacinia bibendum nulla sed consectetur.mollis euismod. Cras mattis consectetur purus sit amet fermentum. </p>
-                        <p>mollis euismod. Cras mattis consectetur purus sit amet fermentum. </p>
-                        <p>Mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.mollis euismod. Cras mattis consectetur purus sit amet fermentum. </p>
+                    <p className="text-left text-dark ml-6">
+                        <p>Experienced Assistant Officer with a demonstrated history of working in the mining & metals industry. Skilled in PHP, Data Entry, CodeIgniter, Bootstrap, and Cascading Style Sheets (CSS). Strong operations professional with a Diploma focused in Computer Science and Information Systems from Universitas Gadjah Mada (UGM). </p>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). </p>
+                        <p> It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. </p>
                     </p>
                 </div>
             </section>
